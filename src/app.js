03fe3d5c7f75d59febbce90b776fce530ca663bd
Mohammad zaid <mohammadzaid8178@gmail.com>
2026-08-23 10:18:39 +0530

    Healty Route added

--- src/app.js
+++ src/app.js
@@ -5,13 +5,15 @@ const { taskRouter } = require("./routes/task.route");
 const cors = require("cors");
 const app = express();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  }),
-);
+app.use(cors());
 app.use(express.json());
+
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "ok",
+    message: "Server is healthy",
+  });
+});
 
 //Router for Users
 app.use("/api/v1/user", userRouter);
